ba96984fbf8b410e574ec7b336564f365844ae69
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-02-06 12:46:49 +0000

    added calcuations for succeptibility and heat capacity

--- checkpoint_1/find_obs_average.py
+++ checkpoint_1/find_obs_average.py
@@ -13,22 +13,28 @@ else:
 data= []
 for i in sorted(os.listdir(f'raw_{flag}_data')):
     data.append(np.loadtxt(str(f'raw_{flag}_data/'+i), delimiter= ',').T)
-print(sorted(os.listdir(f'raw_{flag}_data')))
+
 
 temperature= np.arange(1, 3.1, 0.1)
 
 
 energy= []
 magnetisation= []
+succeptibility= []
+heat_capacity= []
 
 for i in range(len(data)):
     energy.append(np.average(data[i][0][:]))
     magnetisation.append(np.average(np.absolute(data[i][1][:])))
+    succeptibility.append((np.average(np.absolute(data[i][1][:])**2) - np.average(np.absolute(data[i][1][:]))**2)/(2500*temperature[i]))
+    heat_capacity.append((np.average(np.absolute(data[i][0][:])**2) - np.average(np.absolute(data[i][0][:]))**2)/(2500*temperature[i]))
 
 
-fig, (ax1, ax2)= plt.subplots(2, 1, sharex= True)
+fig, (ax1, ax2, ax3, ax4)= plt.subplots(4, 1)
 ax1.scatter(temperature, energy)
 ax2.scatter(temperature, magnetisation)
+ax3.plot(temperature, succeptibility)
+ax4.plot(temperature, heat_capacity)
 plt.show()
 
 
