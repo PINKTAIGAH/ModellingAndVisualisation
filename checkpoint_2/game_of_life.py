fa8a060415daf02facb638704933a73d7e722e23
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-02-12 17:45:08 +0000

    fixed lattice generation

--- checkpoint_2/game_of_life.py
+++ checkpoint_2/game_of_life.py
@@ -5,11 +5,13 @@ import matplotlib.pyplot as plt
 def generate_lattice():
     # Generate initial latttice with random distribution of states 
     global lattice
-    lattice= np.random.choice([0,1], size= (N,N))
-	
+    lattice= np.random.choice(np.array([-1, 1]), size=(N,N))
+
 def periodic_boundaries(neighbours):
     # Apply periodic boundaries to neighbour indices if necesary
+    print(neighbours)
     for i in range(len(neighbours)):
+        print(neighbours[i][0])
         if (neighbours[i][0] +1) % N > 1:
             neighbours[i][0]= -1
         if (neighbours[i][0] -1) % N >1:
@@ -73,8 +75,8 @@ def draw_image(im):
 
 def run_simulation():
     # Initialise game of life simulaiton
-    fig, im= initialise_plot()
     generate_lattice()
+    fig, im= initialise_plot()
     im= draw_image(im)
     # Compute and plot each uptade in state
     while True:
@@ -87,9 +89,8 @@ def main():
         print('Run file in command line as ==>\npython3 game_of_life.py N')
     
     global N
-    N= sys.argv[1]
+    N= int(sys.argv[1])
     run_simulation()    
     
 if __name__== '__main__':
     main()
-
