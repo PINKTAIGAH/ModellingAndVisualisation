fcf776eef236c4f551bc67ae1e58f9b520ed0c0b
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-03-06 18:59:12 +0000

    Imporved histogram plot

--- checkpoint_2/game_of_life/plot_steady_state_hist.py
+++ checkpoint_2/game_of_life/plot_steady_state_hist.py
@@ -2,5 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 dataset= np.loadtxt('equilibration_time_dataset.txt').T
-plt.hist(dataset, bins=50)
+plt.hist(dataset[dataset<4000], bins=50)
+plt.xlabel('Equilibration time, t$_{eq}$ (a.u)')
+plt.ylabel('Frequnecy (counts)')
+plt.title('Equilibration time of random 50 x 50 lattice')
 plt.show()
