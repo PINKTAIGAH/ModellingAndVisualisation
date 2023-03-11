35d38c6751c774164654801937445b99bf68fd3d
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-03-11 12:57:27 +0000

    added resampling method

--- checkpoint_2/SIRS/phase_diagram_plotting.py
+++ checkpoint_2/SIRS/phase_diagram_plotting.py
@@ -9,10 +9,14 @@ data= []
 for i in sorted(os.listdir(f'Data'))[:-1]:
     file_name= f'Data/{i}'
     indv_data= np.loadtxt(file_name).T
-    indv_data_averages= np.average(indv_data/2500, axis=1)
+    indv_data_averages= np.average(indv_data, axis=1)/2500
     data.append(indv_data_averages)
 
-data= np.flip(np.array(data), axis=0)
+data= np.flip(np.array(data).T, axis=0)
 print(data.shape)
-plt.imshow(data, extent=[0,1,0,1])
+plt.imshow(data, extent=[0,1,0,1], cmap='magma')
+plt.colorbar()
+plt.xlabel('Infection probability, p$_1$')
+plt.ylabel('Succeptibility probability, p$_3$')
+plt.title('Recovery probability phase diagram')
 plt.show()
--- checkpoint_2/SIRS/wave_plot_analysis.py
+++ checkpoint_2/SIRS/wave_plot_analysis.py
@@ -1,7 +1,36 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def one():
+    total_energy_resampled_data= []
+    for i in range(len(data)):
+        temp_resampled_data= []
+        for j in range(k):
+            bootstrap_resample= np.random.choice(data[i][0][:], size= n)
+            temp_resampled_data.append(bootstrap_resample)
+        total_energy_resampled_data.append(temp_resampled_data)
+    return np.array(total_energy_resampled_data)
+
+
+def find_resampled_variance(data, n=10000, k=10000):
+    total_resampled_data= []
+    for i in range(data.shape[0]):
+        infected_resampled_data= []
+        for j in range(k):
+            bootstrap_resample= np.random.choice(data[i][:], size=n)
+            infected_resampled_data.append(bootstrap_resample)
+        total_resampled_data.append(infected_resampled_data)
+    return np.array(total_resampled_data)
+
+
+
+
+
 data= np.loadtxt('Data/wave_contour_data.txt').T
+resample_data= find_resampled_variance(data)
 data_reduced= [data[i].var()/2500 for i in range(data.shape[0])] 
 plt.plot(np.arange(0.2,0.51,0.01), data_reduced)
+plt.title('Infection fraction varience vs Infection probability')
+plt.xlabel('Infection probabiolity, p$_1$')
+plt.ylabel('Variance of infection fraction')
 plt.show()
